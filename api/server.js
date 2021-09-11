0bad0aca0ecbed22a76880eaa070f407e0363e08
Henrique Batista Barbosa <henriquebarbosa@ioasys.com.br>
2021-09-11 12:32:39 -0300

    Remove passort and add Procfile

--- api/server.js
+++ api/server.js
@@ -2,12 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import routes from "./routes/routes.js";
-import passport from "passport";
-import googleStrategy from "passport-google-oauth20";
 import cors from "cors";
 
 dotenv.config();
-const GoogleStrategy = googleStrategy.Strategy;
 const server = express();
 server.use(cors());
 server.use(express.json({ limit: "50mb", extended: true }));
@@ -24,23 +21,7 @@ mongoose.connect(
 );
 
 server.use("/api/v1/", routes);
-server.use(passport.initialize());
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "https://pet-id-ufmg.herokuapp.com/home",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
 
 server.listen(process.env.PORT || 9000, () => {
-  console.log(`listening on port ${process.env.PORT || 3000}`);
+  console.log(`listening on port ${process.env.PORT || 9000}`);
 });
